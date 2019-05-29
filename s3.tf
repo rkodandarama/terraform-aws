db0611d183cb9e6b8b7a5c15050423627bd28158
Raghavendra Kodandarama <raghavendra.kodandarama@gmail.com>
2019-05-29 18:47:23 +0530

    formatting

--- s3.tf
+++ s3.tf
@@ -1,7 +1,6 @@
 resource "aws_s3_bucket" "main" {
     bucket = "${var.s3_bucket_name}"
-    acl = "private"
-    tags = "${local.s3_tags}"
-    
+    acl    = "private"
+    tags   = "${local.s3_tags}"
     region = "${var.s3_regions[0]}"
 }
--- variables.tf
+++ variables.tf
@@ -1,35 +1,35 @@
 variable "environment" {
-    type = "string"
-    default = "test"
+  type    = "string"
+  default = "test"
 }
 
 variable "s3_bucket_name" {
 
-    #default = "dscs-test-bucket"
-    description = "Terraform handson first bucket"
-    type = "string"
+  #default = "dscs-test-bucket"
+  description = "Terraform handson first bucket"
+  type        = "string"
 }
 
 #variable "s3_tags" {
 
- #   type = "map"
+#   type = "map"
 
-  #  default = {
-  #      created_by = "Terraform"
-  #      environment = "${var.environment}"
-  #  }
+#  default = {
+#      created_by = "Terraform"
+#      environment = "${var.environment}"
+#  }
 #}
 
 locals {
-   s3_tags = {
-       created_by = "terraform"
-       environment = "${var.environment}"
-   }
+  s3_tags = {
+    created_by  = "terraform"
+    environment = "${var.environment}"
+  }
 
 }
 
 variable "s3_regions" {
 
-    type = "list"
-    default = ["ap-southeast-2", "us-west-2"]
+  type    = "list"
+  default = ["ap-southeast-2", "us-west-2"]
 }
