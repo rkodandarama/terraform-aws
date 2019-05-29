9ab3bc8bbc6e91e2724d9f0cbe5049b5bf8bab66
Raghavendra Kodandarama <raghavendra.kodandarama@gmail.com>
2019-05-29 18:19:43 +0530

    working with locals

--- s3.tf
+++ s3.tf
@@ -1,9 +1,7 @@
 resource "aws_s3_bucket" "main" {
     bucket = "${var.s3_bucket_name}"
     acl = "private"
-    tags = {
-        env = "${lookup(var.s3_tags, "environment")}"
-    }
+    tags = "${local.s3_tags}"
     
     region = "${var.s3_regions[0]}"
 }
